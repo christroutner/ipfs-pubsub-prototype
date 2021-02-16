152fc7b25715a661c76fe36fa3aedefcd7cdea96
Chris Troutner <chris.troutner@gmail.com>
2021-02-15 18:05:36 -0800

    linting node.js app

--- node.js/ipfs-chat-bot.js
+++ node.js/ipfs-chat-bot.js
@@ -9,12 +9,12 @@
 //   "/ip4/116.203.193.74/tcp/4001/ipfs/QmNZktxkfScScnHCFSGKELH3YRqdxHQ3Le9rAoRLhZ6vgL";
 
 // Pubsub room.
-const CHAT_ROOM_NAME = 'psf-ipfs-chat-001';
+const CHAT_ROOM_NAME = "psf-ipfs-chat-001";
 
 // Global npm libraries
 const IPFS = require("ipfs");
 const IpfsCoord = require("ipfs-coord")
-// const IpfsCoord = require('../../ipfs-coord/index')
+// const IpfsCoord = require("../../ipfs-coord/index");
 
 let ipfsId; // Used to track the IPFS ID of this node.
 let ipfs; // instance of IPFS for this node.
@@ -45,34 +45,38 @@ async function startClientNode() {
     console.log("... IPFS is ready.\n");
 
     // Instantiate the IPFS Coordination library.
-    ipfsCoord = new IpfsCoord({ipfs, type: 'node.js'})
-    await ipfsCoord.isReady()
-    console.log('IPFS coordination is ready.')
-
+    ipfsCoord = new IpfsCoord({ ipfs, type: "node.js", isCircuitRelay: true });
+    await ipfsCoord.isReady();
+    console.log("IPFS coordination is ready.");
 
     // subscribe to the 'chat' chatroom.
-    await ipfsCoord.ipfs.pubsub.subscribeToPubsubChannel(CHAT_ROOM_NAME, handleChat)
+    await ipfsCoord.ipfs.pubsub.subscribeToPubsubChannel(
+      CHAT_ROOM_NAME,
+      handleChat
+    );
 
     // Send a chat message to the chat room.
     setInterval(async function() {
-      const now = new Date()
-      const msg = `Message from chat bot at ${now.toLocaleString()}`
-      const handle = 'chat bot'
+      const now = new Date();
+      const msg = `Message from chat bot at ${now.toLocaleString()}`;
+      const handle = "chat bot";
       const chatObj = {
         message: msg,
         handle: handle
-      }
+      };
 
       // Add the chat data to the schema.
-      const chatData = ipfsCoord.ipfs.schema.chat(chatObj)
+      const chatData = ipfsCoord.ipfs.schema.chat(chatObj);
 
       // Convert the chat JSON object into a string.
-      const chatDataStr = JSON.stringify(chatData)
+      const chatDataStr = JSON.stringify(chatData);
 
       // Publish the stringified chat object to the pubsub channel.
-      await ipfsCoord.ipfs.pubsub.publishToPubsubChannel(CHAT_ROOM_NAME, chatDataStr)
-    }, 60000*5) // five minutes
-
+      await ipfsCoord.ipfs.pubsub.publishToPubsubChannel(
+        CHAT_ROOM_NAME,
+        chatDataStr
+      );
+    }, 60000 * 5); // five minutes
   } catch (err) {
     console.error("Error: ", err);
   }
@@ -82,11 +86,10 @@ startClientNode();
 function handleChat(msgData) {
   // console.log('msgData: ', msgData)
 
-  let from = msgData.from
-  if(msgData.data.data.handle)
-    from = msgData.data.data.handle
+  let from = msgData.from;
+  if (msgData.data.data.handle) from = msgData.data.data.handle;
 
-  console.log(`Peer ${from} says: ${msgData.data.data.message}`)
+  console.log(`Peer ${from} says: ${msgData.data.data.message}`);
 }
 
 // Promise based sleep function:
