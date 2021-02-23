387524c41353eb395f25a260222555af3c2fc295
Chris Troutner <chris.troutner@gmail.com>
2021-02-22 22:00:24 -0800

    Successfully sent encrypted message between node.js and second node

--- second-node/mock-client.js
+++ second-node/mock-client.js
@@ -1,18 +1,16 @@
 /*
-  A node.js IPFS node. This could represent a REST API server.
+  A second node.js IPFS peer. It's often faster to prototype between two node.js
+  peers, than a node.js peer and a browser peer.
 */
 
 const BCHJS = require('@psf/bch-js')
 const bchjs = new BCHJS()
 
-// Relay nodes.
-// const CHAT_ADDR =
-//   "/ip4/138.68.212.34/tcp/4002/ipfs/QmaUW4oCVPUFLRqeSjvhHwGFJHGWrYWLBEt7WxnexDm3Xa";
-// const BOOTSTRAP_ADDR =
-//   "/ip4/116.203.193.74/tcp/4001/ipfs/QmNZktxkfScScnHCFSGKELH3YRqdxHQ3Le9rAoRLhZ6vgL";
-
 // Pubsub room.
 const CHAT_ROOM_NAME = "psf-ipfs-chat-001";
+
+// Set these constants for your own tests.
+const PORT = 6002
 
 // Global npm libraries
 const IPFS = require("ipfs");
@@ -24,7 +22,7 @@ let ipfs; // instance of IPFS for this node.
 
 // Ipfs Options
 const ipfsOptions = {
-  repo: "./chatdata",
+  repo: "./ipfs-data",
   start: true,
   EXPERIMENTAL: {
     pubsub: true
@@ -36,7 +34,12 @@ const ipfsOptions = {
         enabled: true // enable circuit relay HOP (make this node a relay)
       }
     },
-    pubsub: true // enable pubsub
+    pubsub: true, // enable pubsub
+    Addresses: {
+      Swarm: [`/ip4/0.0.0.0/tcp/${PORT}`],
+      API: `/ip4/127.0.0.1/tcp/${PORT+1}`,
+      Gateway: `/ip4/127.0.0.1/tcp/${PORT+2}`
+    }
   }
 };
 
@@ -48,7 +51,7 @@ async function startClientNode() {
     console.log("... IPFS is ready.\n");
 
     // Instantiate the IPFS Coordination library.
-    ipfsCoord = new IpfsCoord({ ipfs, bchjs, BCHJS, type: "node.js", isCircuitRelay: true });
+    ipfsCoord = new IpfsCoord({ ipfs, bchjs, BCHJS, type: "node.js", isCircuitRelay: false });
     await ipfsCoord.isReady();
     console.log("IPFS coordination is ready.");
 
@@ -61,8 +64,8 @@ async function startClientNode() {
     // Send a chat message to the chat room.
     setInterval(async function() {
       const now = new Date();
-      const msg = `Message from chat bot at ${now.toLocaleString()}`;
-      const handle = "chat bot";
+      const msg = `Message from Mock Client at ${now.toLocaleString()}`;
+      const handle = "mock client";
       const chatObj = {
         message: msg,
         handle: handle
@@ -81,32 +84,6 @@ async function startClientNode() {
       );
     }, 60000 * 5); // five minutes
 
-    // Periodically list each peer and send an e2e encypted message to them.
-    setInterval(async function () {
-      try {
-        const peers = ipfsCoord.ipfs.peers.state.peers
-        const peerList = ipfsCoord.ipfs.peers.state.peerList
-        // console.log(`Peers: ${JSON.stringify(peers, null, 2)}`)
-        // console.log(`peerList.length: ${peerList.length}`)
-
-        const secondNode = 'QmaiNMyvNng12H4iUwuSnHGc3ojeZaf3TRzy3iBmC1hQft'
-
-        if(peerList.indexOf(secondNode) < 0) {
-          console.log(`Second node peer ${secondNode} not yet connected`)
-          return
-        }
-
-        // const thisPeer = peerList[0]
-        // console.log(`Sending e2e encrypted message to this peer: ${thisPeer}`)
-
-        const msg = 'hello there!'
-        // await ipfsCoord.ipfs.encrypt.sendEncryptedMsg(peers[thisPeer], msg)
-        await ipfsCoord.ipfs.encrypt.sendEncryptedMsg(peers[secondNode], msg)
-
-      } catch(err) {
-        console.error('Error trying to send e2e encrypted message to each peer: ', err)
-      }
-    }, 1000 * 20)
   } catch (err) {
     console.error("Error: ", err);
   }
